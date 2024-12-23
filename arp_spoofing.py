e45faf43004c266ec5eeaae2a3f10b54c10de567
r0man22 <roman.mammadov22@gmail.com>
2024-12-23 14:24:08 -0500

    device name

--- arp_spoofing.py
+++ arp_spoofing.py
@@ -1,6 +1,17 @@
 import subprocess
 
-result = subprocess.run(["fping", "-a", "-g", "192.168.1.0/24"], capture_output=True, text=True)
+result_fping = subprocess.run(["fping", "-a", "-g", "192.168.1.0/24"], capture_output=True, text=True)
 
 address_list = result.stdout.splitlines()
-print(address_list)
+
+for index, item in enumerate(address_list, start=1):
+	print(f"{index}. {item}")
+
+result_nmap = subprocess.run(['nmap', '-sn', '192.168.1.0/24'], capture_output=True, text=True)
+
+lines = result.stdout.split('\n')
+
+nmap_reports = [line for line in lines if 'Nmap scan report for' in line]
+
+device_name = [line.split()[4] for line in nmap_reports]
+
